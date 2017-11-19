95062eee8e20f7de0afd576f9777f16c562b4476
thomas <thomas@thomas-W65-W670SR>
2017-11-19 19:48:13 +0100

    hierarchical mapping, just with gmapping and octomapping and list of maps

--- semantic_mapping_v2/include/semantic_mapping_v2/HierarchyMapper.h
+++ semantic_mapping_v2/include/semantic_mapping_v2/HierarchyMapper.h
@@ -7,4 +7,38 @@
 
 #include "semantic_mapping_v2/RoomMapper.h"
 
+class HierarchyMapper{
+  protected:
+    std::vector<RoomMapper*> room_mapper_;
+    int current_mapper_ = -1;
+
+    ros::Subscriber laser_sub_;
+    ros::Subscriber cloud_sub_;
+
+    ros::Publisher map_pub_;
+    ros::Publisher map_info_pub_;
+    ros::Publisher marker_pub_;
+
+    tf::TransformBroadcaster* tfB_;
+    ros::NodeHandle nh_;
+    boost::thread* transform_thread_;
+
+    void transformPublishLoop(double transform_publish_period);
+
+    double transform_publish_period_;
+
+  public:
+    HierarchyMapper();
+
+    void addMapper(bool do_switch = true);
+    void switchMapper(int mapper_idx);
+
+    virtual void cloudCb(const sensor_msgs::PointCloud2::ConstPtr& cloud);
+    void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+
+    void publish();
+
+    void run();
+};
+
 #endif //SEMANTIC_MAPPING_V2_HIERARCHYMAPPER_H
--- semantic_mapping_v2/src/HierarchyMapper.cpp
+++ semantic_mapping_v2/src/HierarchyMapper.cpp
@@ -2,3 +2,77 @@
 // Created by thomas on 19.11.17.
 //
 
+#include "semantic_mapping_v2/HierarchyMapper.h"
+
+
+HierarchyMapper::HierarchyMapper(){
+  addMapper(true);
+
+  laser_sub_ = nh_.subscribe("scan", 1, &HierarchyMapper::cloudCb, this);
+  cloud_sub_ = nh_.subscribe("/camera/depth_registered/points", 1, &HierarchyMapper::cloudCb, this);
+
+  map_pub_ = nh_.advertise<nav_msgs::OccupancyGrid>("map", 1, true);
+  map_info_pub_ = nh_.advertise<nav_msgs::MapMetaData>("map_metadata", 1, true);
+  marker_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("occupied_cells_vis_array", 1, true);
+
+  ros::NodeHandle("~").param("transform_publish_period", transform_publish_period_, 0.05);
+  tfB_ = new tf::TransformBroadcaster();
+  transform_thread_ = new boost::thread(boost::bind(&HierarchyMapper::transformPublishLoop, this, transform_publish_period_));
+}
+
+
+void HierarchyMapper::addMapper(bool do_switch){
+  room_mapper_.push_back(new RoomMapper);
+  if(do_switch)
+    switchMapper(room_mapper_.size() - 1);
+}
+
+
+void HierarchyMapper::switchMapper(int mapper_idx){
+  current_mapper_ = mapper_idx;
+}
+
+
+void HierarchyMapper::cloudCb(const sensor_msgs::PointCloud2::ConstPtr& cloud){
+  if(current_mapper_ >= 0 && current_mapper_ < room_mapper_.size())
+    room_mapper_[current_mapper_]->cloudCb(cloud);
+}
+
+
+void HierarchyMapper::laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+  if(current_mapper_ >= 0 && current_mapper_ < room_mapper_.size())
+    room_mapper_[current_mapper_]->laserCallback(scan);
+}
+
+
+void HierarchyMapper::transformPublishLoop(double transform_publish_period){
+  if(transform_publish_period == 0)
+    return;
+
+  ros::Rate r(1.0 / transform_publish_period);
+  while(ros::ok()){
+    if(current_mapper_ >= 0 && current_mapper_ < room_mapper_.size()){
+      tfB_->sendTransform(room_mapper_[current_mapper_]->getTransform());
+    }
+    r.sleep();
+  }
+}
+
+
+void HierarchyMapper::publish(){
+  map_pub_.publish(room_mapper_[current_mapper_]->getMap());
+  map_info_pub_.publish(room_mapper_[current_mapper_]->getMap().info);
+  marker_pub_.publish(room_mapper_[current_mapper_]->getOccupiedCellMsg());
+}
+
+
+void HierarchyMapper::run(){
+  ros::Rate rate(50);
+  while(ros::ok()){
+    ros::spinOnce();
+    if(current_mapper_ >= 0 && current_mapper_ < room_mapper_.size() && room_mapper_[current_mapper_]->resetWasMapUpdated())
+      publish();
+    rate.sleep();
+  }
+}
+
