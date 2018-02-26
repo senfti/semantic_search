f00885af90c4e6a6272f868105a79c5f95d6d129
thomas <thomas@thomas-W65-W670SR>
2018-02-26 12:39:41 +0100

    odom to tf node (so no tf recording needed in rosbag)

---
+++ odom_to_tf/src/main.cpp
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+tf::TransformBroadcaster* odom_broadcaster;
+std::string odom_topic = "odom";
+std::string odom_link = "odom";
+std::string robot_link = "base_link";
+
+void odomCb(const nav_msgs::OdometryConstPtr& msg){
+  geometry_msgs::TransformStamped odom_trans;
+  odom_trans.header.frame_id = odom_link;
+  odom_trans.child_frame_id = robot_link;
+  odom_trans.header.stamp = msg->header.stamp;
+  odom_trans.header.seq = msg->header.seq;
+  odom_trans.transform.translation.x = msg->pose.pose.position.x;
+  odom_trans.transform.translation.y = msg->pose.pose.position.y;
+  odom_trans.transform.translation.z = msg->pose.pose.position.z;
+  odom_trans.transform.rotation.x = msg->pose.pose.orientation.x;
+  odom_trans.transform.rotation.y = msg->pose.pose.orientation.y;
+  odom_trans.transform.rotation.z = msg->pose.pose.orientation.z;
+  odom_trans.transform.rotation.w = msg->pose.pose.orientation.w;
+
+  odom_broadcaster->sendTransform(odom_trans);
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "odom_to_tf");
+
+  if(argc >= 4){
+    odom_topic = argv[1];
+    odom_link = argv[2];
+    robot_link = argv[3];
+  }
+
+  ros::NodeHandle n;
+  tf::TransformBroadcaster odom_broadcaster_tmp;
+  odom_broadcaster = &odom_broadcaster_tmp;
+
+  ros::Subscriber odom_sub = n.subscribe("/"+odom_topic, 1, odomCb);
+  ros::Rate rate(20.0);
+  while(ros::ok()){
+    ros::spinOnce();
+    rate.sleep();
+  }
+}
